88e73b1b64f7079897e66001baaefdd407c0df44
amaninders <amandeolofficial@gmail.com>
2021-08-16 21:45:57 +0000

    First push after writing the code

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(a,b) {
+  console.log((a === b) ? '✅✅✅ Assertion Passed: ' + a + ' === ' + b : '🔴🔴🔴 Assertion Failed: ' + a + ' !== ' + b);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs","Lighthouse Labs");
+assertEqual(1,1);
+assertEqual("Lighthouse Labs","Bootcamp");
+assertEqual(1,3);
