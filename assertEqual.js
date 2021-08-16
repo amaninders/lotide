a696ed918aef86b737dd19b22be7ea96c45ea2e5
amaninders <amandeolofficial@gmail.com>
2021-08-16 22:38:41 +0000

    removed the additional spacing/newlines after function definition

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,6 @@ const assertEqual = function(a,b) {
   console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
 };
 
-
 // TEST CODE
 assertEqual("Lighthouse Labs","Bootcamp");
 assertEqual(1,1);
