6638f122731eada6d94346bfec8e0c0d2e982f77
amaninders <amandeolofficial@gmail.com>
2021-08-19 09:08:25 +0000

    replaced if-else block with ternary

--- countOnly.js
+++ countOnly.js
@@ -10,11 +10,7 @@ const countOnly = function (allItems, itemsToCount) {
 
   for (const item of allItems) {
     if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }      
+      (results[item]) ? results[item] += 1 : results[item] = 1;
     }
   }
   return results;
