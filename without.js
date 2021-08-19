390b27a99f8c173db25784c5fdc504204630e75c
amaninders <amandeolofficial@gmail.com>
2021-08-19 08:36:40 +0000

    corrected the test case and used assertArraysEqual

--- without.js
+++ without.js
@@ -1,16 +1,14 @@
-// function to compare two inputs
-const assertEqual = function(a,b) {
-  console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
-};
-
-// function to compare the arrays
-const assertArraysEqual = (arr1, arr2) => {
-
+// TEST/ASSERTION FUNCTIONS
+const eqArrays = (arr1, arr2) => {
   // compare length of arrays then do element match with anonymous function
-  console.log(`Original array is intact:`,(arr1.length === arr2.length) ? arr1.every((element,index) => element === arr2[index]) : false);
+  return (arr1.length === arr2.length) ? arr1.every((element,index) => element === arr2[index]) : false;
 };
 
-// function to remove the array items
+const assertArraysEqual = function(a,b) {
+  console.log((eqArrays(a, b)) ? `✅ Assertion Passed: [${a}] === [${b}]` : `🔴 Assertion Failed: [${a}] !== [${b}]`);
+};
+
+// ACTUAL FUNCTION
 const without = (source, itemsToRemove) => {
   
   // initialize empty array to store the output
@@ -18,14 +16,15 @@ const without = (source, itemsToRemove) => {
 
   // loop through array elements and push to output if found in itemsToRemove
   for (const element of source) {
-    if(itemsToRemove.indexOf(element) < 0 ) {
+    if (itemsToRemove.indexOf(element) < 0) {
       output.push(element);
-    };
+    }
   }
-  console.log(`New Array:`, output);
+  return output;
 };
 
 // TEST CODE
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
-assertArraysEqual(words,["hello", "world", "lighthouse"]);
+assertArraysEqual(words,["hello", "world", "lighthouse"]); //verify actual array
+assertArraysEqual(without(words,["hello", "world"]),["lighthouse"]); // should be true
