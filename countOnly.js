6ebe69ba1b362f3e076a1f2af2679b724043a5e5
amaninders <amandeolofficial@gmail.com>
2021-08-19 00:16:39 +0000

    completed the countOnly function

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+// TEST/ASSERTION FUNCTIONS
+const assertEqual = function(a,b) {
+  console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
+};
+
+// ACTUAL FUNCTION
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }      
+    }
+  }
+  return results;
+}
+
+
+// TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
