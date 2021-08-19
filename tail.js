7a47c900322e5514afec1f2cddcbaf925f3ccac6
amaninders <amandeolofficial@gmail.com>
2021-08-19 08:04:50 +0000

    corrected the test code

--- tail.js
+++ tail.js
@@ -6,11 +6,14 @@ const tail = function(array) {
   return (array.length <= 1) ? [] : array.slice(1);
 };
 
-// TEST CODE
-console.log(tail([5,6,7,5]));
-console.log(tail([5]));
+// TEST CODE: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case: Check the original array
+
+// TEST CODE: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
