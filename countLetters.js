9f487df829900305d2ff36562d1f3f68da71c699
amaninders <amandeolofficial@gmail.com>
2021-08-19 09:12:31 +0000

    refactored if-else and replaced with ternary: and used dot notation in the testcode to access the object key value

--- countLetters.js
+++ countLetters.js
@@ -8,17 +8,13 @@ const assertEqual = function(a,b) {
 const countLetters = function(string) {
   const results = {};
   for (const letter of string) {
-    if (results[letter]) {
-      results[letter] += 1;
-    } else {
-      results[letter] = 1;
-    }
+    (results[letter]) ? results[letter] += 1 : results[letter] = 1;
   }
   return results;
 };
 
 // TEST CODE
-assertEqual(countLetters('Jason')["M"],undefined); // should be undefined
-assertEqual(countLetters('Brandon')["n"],2); // should be true
-assertEqual(countLetters('Janelle')["J"],1); // should be true
-assertEqual(countLetters('Graham')["a"],2); // should be true
+assertEqual(countLetters('Jason').m, undefined); // should be undefined
+assertEqual(countLetters('Brandon').n, 2); // should be true
+assertEqual(countLetters('Janelle').J, 1); // should be true
+assertEqual(countLetters('Graham').a, 2); // should be true
