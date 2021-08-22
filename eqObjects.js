b612e4972464cf5f6440cdd1f5e697cb07445535
amaninders <amandeolofficial@gmail.com>
2021-08-22 07:57:40 +0000

    added a test code to demonstrate the eqObject fails for nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -39,10 +39,18 @@ const eqObjects = function(object1, object2) {
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, ba), true); // should pass
+assertEqual(eqObjects(ab, abc), false); // should fail
+
+
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(ab, ba),true);
-assertEqual(eqObjects(ab, abc),false);
-assertEqual(eqObjects(cd, dc),true);
-assertEqual(eqObjects(cd, cd2),false);
+assertEqual(eqObjects(cd, dc), true); // should pass
+assertEqual(eqObjects(cd, cd2), false); // should fail
+
+const ef = { 1: "1", 2: { first: 'alpha', second: 'beta', third: 'gamma'}};
+const eg = { 1: "1", 2: ['alpha', 'beta', 'gamma']};
+const fg = { 2: { first: 'alpha', second: 'beta', third: 'gamma'}, 1: "1"};
+assertEqual(eqObjects(ef, eg), false); // should fail
+assertEqual(eqObjects(ef, fg), true); //should pass
