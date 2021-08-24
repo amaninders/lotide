8ca03b81bb930b8fda0730013385910c72dd0cf2
amaninders <amandeolofficial@gmail.com>
2021-08-24 01:48:44 +0000

    refactore to add module.exports and linting done

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-const assertEqual = function(a,b) {
-  console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
-};
-
 const head = function(array) {
   return (array.length) ? array[0] : 'undefined';
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
