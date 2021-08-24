1e6a82415fe2b8a90bcb6396a8b6a4b1cba755ae
amaninders <amandeolofficial@gmail.com>
2021-08-24 02:04:12 +0000

    completed linting and exported the module

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,15 @@
-// function to compare the arrays
-const eqArrays = (arr1, arr2) => {
-  // compare length of arrays then do element match with anonymous function
-  return (arr1.length === arr2.length) ? arr1.every((element,index) => element === arr2[index]) : false;
-};
+const eqArrays = require('./eqArrays');
 
 // function to compare two inputs
 const assertArraysEqual = function(a,b) {
-  console.log((eqArrays(a, b)) ? `✅ Assertion Passed: [${a}] === [${b}]` : `🔴 Assertion Failed: [${a}] !== [${b}]`);
+
+  const success = `✅ Assertion Passed: [${a}] === [${b}]`;
+  const failure = `🔴 Assertion Failed: [${a}] !== [${b}]`;
+  console.log((eqArrays(a, b) ? success  : failure));
+
+  // uncomment the following to return an output true/false
+  return (eqArrays(a, b)) ? true : false;
+
 };
 
-
-// TEST CODE
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
-console.log(assertArraysEqual([1, 2, 3], [1, 3, 3])); // => true
-console.log(assertArraysEqual([1, 2, 3], [1, 2])); // => true
+module.exports = assertArraysEqual;
