3a456598e0ec813e3b02397db0ed0d7504a8b777
amaninders <amandeolofficial@gmail.com>
2021-08-24 03:38:17 +0000

    implemented the module export

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const assertEqual = function(a,b) {
-  console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
-};
-
-// ACTUAL FUNCTION
-
 const countLetters = function(string) {
   const results = {};
   for (const letter of string) {
@@ -13,8 +6,4 @@ const countLetters = function(string) {
   return results;
 };
 
-// TEST CODE
-assertEqual(countLetters('Jason').m, undefined); // should be undefined
-assertEqual(countLetters('Brandon').n, 2); // should be true
-assertEqual(countLetters('Janelle').J, 1); // should be true
-assertEqual(countLetters('Graham').a, 2); // should be true
+module.exports = countLetters;
