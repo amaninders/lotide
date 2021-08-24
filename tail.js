72f27686aecd33c57d9a8063387420f539981426
amaninders <amandeolofficial@gmail.com>
2021-08-24 01:53:35 +0000

    refactored to add the export module and linting done

--- tail.js
+++ tail.js
@@ -1,19 +1,5 @@
-const assertEqual = function(a,b) {
-  console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
-};
-
 const tail = function(array) {
-  return (array.length <= 1) ? [] : array.slice(1);
+  return (array.length <= 1) ? [] : array.slice(1); //optimize and use only .slice
 };
 
-// TEST CODE: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
-// TEST CODE: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
