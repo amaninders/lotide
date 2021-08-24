3470b545115cdd6a81b4b092bc845deed97c522e
amaninders <amandeolofficial@gmail.com>
2021-08-24 02:59:28 +0000

    refactored the code and implemented export

--- flatten.js
+++ flatten.js
@@ -1,27 +1,22 @@
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = (arr1, arr2) => {
-  // compare length of arrays then do element match with anonymous function
-  return (arr1.length === arr2.length) ? arr1.every((element,index) => element === arr2[index]) : false;
-};
-
-const assertArraysEqual = function(a,b) {
-  console.log((eqArrays(a, b)) ? `✅ Assertion Passed: [${a}] === [${b}]` : `🔴 Assertion Failed: [${a}] !== [${b}]`);
-};
-
 // ACTUAL FUNCTION
 const flatten = array => {
 
   // initialize the output array
-  let output = [];
+  const output = [];
 
-  // execute for loop to generate the output
-  for (let item of array) {
-    Array.isArray(item) ? output = [...output,...item] : output.push(item);
-  }
+  const flatChild = (childArray) => {
+    // execute for loop to generate the output
+    for (const item of childArray) {
+      if (Array.isArray(item)) {
+        flatChild(item);
+      } else {
+        output.push(item);
+      }
+    }
+  };
+
+  flatChild(array);
   return output;
 };
 
-// TEST CODE
-const testArray = [1, 2, [3, 4], 5, [6]];
-const expectedArray =  [1, 2, 3, 4, 5, 6];
-assertArraysEqual(flatten(testArray), expectedArray); // should pass the assertion
+module.exports = flatten;
