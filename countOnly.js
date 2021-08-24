c444936bb8240530be3eeb8f1ca0598aa866df15
amaninders <amandeolofficial@gmail.com>
2021-08-24 03:16:29 +0000

    implemented the module export

--- countOnly.js
+++ countOnly.js
@@ -1,8 +1,3 @@
-// TEST/ASSERTION FUNCTIONS
-const assertEqual = function(a,b) {
-  console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
-};
-
 // ACTUAL FUNCTION
 
 const countOnly = function(allItems, itemsToCount) {
@@ -16,23 +11,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-
-// TEST CODE
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
