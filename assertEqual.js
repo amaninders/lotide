5064fc45b1a55ee7d790fef56477deee0d87d61e
amaninders <amandeolofficial@gmail.com>
2021-08-24 01:39:45 +0000

    cleaned the function and linting done

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,12 @@
 const assertEqual = function(a,b) {
-  console.log((a === b) ? `✅ Assertion Passed: ${a} === ${b}` : `🔴 Assertion Failed: ${a} !== ${b}`);
+	
+  // uncomment the following loc if you need a visual output for the result
+  const success = `✅ Assertion Passed: [${a}] === [${b}]`;
+  const failure = `🔴 Assertion Failed: [${a}] !== [${b}]`;
+  console.log((a === b) ?  success : failure);
+
+  // uncomment the following line to return an output for assertion
+  // return (a === b) ? true : false;
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs","Bootcamp");
-assertEqual(1,1);
+module.exports = assertEqual;
